5f177b773dbf67eac9bb04137d0209fc46d6a943
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2026-07-16 00:32:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# FastDS
+
+Made for Blender 5.1.2, might work on versions greater than 3.2 but this will be untested.
+
+This is a Blender plugin made for The Legend of Zelda: Phantom Hourglass and The Legend of Zelda: Spirit Tracks that allows one to import and export Nintendo DS model/collision files.
+
+WIP!
+
+## Developement
+
+We recommend using Astral's `ty` and `ruff` for linting/formatting Python files.
---
+++ __init__.py
@@ -0,0 +1,98 @@
+import bpy
+
+from bpy.utils import register_class, unregister_class
+from .updater import addon_updater_ops
+
+# info about add on
+bl_info = {
+    "name": "FastDS",
+    "version": (0, 0, 1),
+    "author": "Yanis002",
+    "location": "3DView",
+    "description": "Plugin for importing/exporting DS-related data (targetting DS Zelda games).",
+    "category": "Import-Export",
+    "blender": (5, 0, 0),
+}
+
+gameEditorEnum = (
+    ("PH", "PH", "Phantom Hourglass", 0),
+    ("ST", "ST", "Spirit Tracks", 1),
+)
+
+
+class FastDS_GlobalToolsPanel(bpy.types.Panel):
+    bl_idname = "FASTDS_PT_global_tools"
+    bl_label = "FastDS Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "FastDS"
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    # called every frame
+    def draw(self, context):
+        assert self.layout is not None
+        self.layout.label(text="Empty for now.")
+        addon_updater_ops.update_notice_box_ui(self, context)
+
+
+class ExampleAddonPreferences(bpy.types.AddonPreferences, addon_updater_ops.AddonUpdaterPreferences):
+    bl_idname = __package__
+
+    def draw(self, context):
+        addon_updater_ops.update_settings_ui(self, context)
+
+
+classes = (FastDS_GlobalToolsPanel,)
+
+
+@bpy.app.handlers.persistent
+def after_load(_a, _b):
+    # Doing some operations immediately on file load can crash blender in specific situations,
+    # so delay the post-load code execution.
+    # (note if register() is called without a delay the function just runs immediately, so we need any non-zero delay)
+    bpy.app.timers.register(after_load_impl, first_interval=0.001)
+
+
+def after_load_impl():
+    pass
+
+
+# called on add-on enabling
+# register operators and panels here
+# append menu layout drawing function to an existing window
+def register():
+    if bpy.app.version < (3, 2, 0):
+        msg = "\n".join(
+            (
+                "This version of FastDS does not support Blender 3.1.x and earlier Blender versions.",
+                "Your Blender version is: " + ".".join(str(i) for i in bpy.app.version),
+                "Please upgrade Blender to 3.2.0 or above.",
+            )
+        )
+        print(msg)
+        unsupported_exc = Exception("\n\n" + msg)
+        raise unsupported_exc
+
+    # Register addon updater first,
+    # this way if a broken version fails to register the user can still pick another version.
+    register_class(ExampleAddonPreferences)
+    addon_updater_ops.register(bl_info)
+
+    for cls in classes:
+        register_class(cls)
+
+    bpy.app.handlers.load_post.append(after_load)
+
+
+# called on add-on disabling
+def unregister():
+    for cls in classes:
+        unregister_class(cls)
+
+    bpy.app.handlers.load_post.remove(after_load)
+
+    addon_updater_ops.unregister()
+    unregister_class(ExampleAddonPreferences)
